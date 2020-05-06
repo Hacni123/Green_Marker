16802ccb7bf15886393130ea83724b501e8f90be
Hasini <hasini.b94@gmail.com>
2020-05-06 10:33:46 +0530

    All files in php website

---
+++ customer_register.php
@@ -0,0 +1,263 @@
+<?php 
+
+    $active='Account';
+    include("includes/header.php");
+
+?>
+   
+   <div id="content"><!-- #content Begin -->
+       <div class="container"><!-- container Begin -->
+           <div class="col-md-12"><!-- col-md-12 Begin -->
+               
+               <ul class="breadcrumb"><!-- breadcrumb Begin -->
+                   <li>
+                       <a href="index.php">Home</a>
+                   </li>
+                   <li>
+                       Register
+                   </li>
+               </ul><!-- breadcrumb Finish -->
+               
+           </div><!-- col-md-12 Finish -->
+           
+           <div class="col-md-12"><!-- col-md-12 Begin -->
+               
+               <div class="box"><!-- box Begin -->
+                   
+                   <div class="box-header"><!-- box-header Begin -->
+                       
+                       <center><!-- center Begin -->
+                           
+                           <h2> Register a new account </h2>
+                           
+                       </center><!-- center Finish -->
+                       
+                       <form action="customer_register.php" method="post" enctype="multipart/form-data"><!-- form Begin -->
+                           
+                           <div class="form-group"><!-- form-group Begin -->
+                               
+                               <label>Your Name</label>
+                               
+                               <input type="text" class="form-control" name="c_name" required>
+                               
+                           </div><!-- form-group Finish -->
+                           
+                           <div class="form-group"><!-- form-group Begin -->
+                               
+                               <label>Your Email</label>
+                               
+                               <input type="text" class="form-control" name="c_email" required>
+                               
+                           </div><!-- form-group Finish -->
+                           
+                           <div class="form-group"><!-- form-group Begin -->
+                               
+                               <label>Your Password</label>
+                               
+                               <input type="password" class="form-control" name="c_pass" required>
+                               
+                           </div><!-- form-group Finish -->
+                           
+                           <div class="form-group"><!-- form-group Begin -->
+                               
+                               <label>Your Country</label>
+                               
+                               <input type="text" class="form-control" name="c_country" required>
+                               
+                           </div><!-- form-group Finish -->
+                           
+                           <div class="form-group"><!-- form-group Begin -->
+                               
+                               <label>Your City</label>
+                               
+                               <input type="text" class="form-control" name="c_city" required>
+                               
+                           </div><!-- form-group Finish -->
+                           
+                           <div class="form-group"><!-- form-group Begin -->
+                               
+                               <label>Your Contact</label>
+                               
+                               <input type="text" class="form-control" name="c_contact" required>
+                               
+                           </div><!-- form-group Finish -->
+                           
+                           <div class="form-group"><!-- form-group Begin -->
+                               
+                               <label>Your Address</label>
+                               
+                               <input type="text" class="form-control" name="c_address" required>
+                               
+                           </div><!-- form-group Finish -->
+                           
+                           <div class="form-group"><!-- form-group Begin -->
+                               
+                               <label>Your Profile Picture</label>
+                               
+                               <input type="file" class="form-control form-height-custom" name="c_image" required>
+                               
+                           </div><!-- form-group Finish -->
+                           
+                           <div class="text-center"><!-- text-center Begin -->
+                               
+                               <button type="submit" name="register" class="btn btn-primary">
+                               
+                               <i class="fa fa-user-md"></i> Register
+                               
+                               </button>
+                               
+                           </div><!-- text-center Finish -->
+                           
+                       </form><!-- form Finish -->
+                       
+                   </div><!-- box-header Finish -->
+                   
+               </div><!-- box Finish -->
+               
+           </div><!-- col-md-12 Finish -->
+           
+       </div><!-- container Finish -->
+   </div><!-- #content Finish -->
+   
+   <?php 
+    
+    include("includes/footer.php");
+    
+    ?>
+    
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+    
+    
+</body>
+</html>
+
+
+<?php 
+
+if (isset($_POST["register"])) {
+
+	$c_name = $_POST["c_name"];
+	$c_email = $_POST["c_email"];
+	$c_pass = $_POST['c_pass'];
+	$c_country = $_POST['c_country'];
+	$c_city = $_POST['c_city'];
+	$c_contact = $_POST['c_contact'];
+	$c_address = $_POST['c_address'];
+	$c_image = $_POST['c_image'];
+	 $c_image_tmp = $_FILES['c_image']['tmp_name'];
+	  $c_ip = getRealIpUser();
+	$name = "/^[a-zA-Z ]+$/";
+	$emailValidation = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9]+(\.[a-z]{2,4})$/";
+	$number = "/^[0-9]+$/";
+
+if(empty($c_name) || empty($c_email) || empty($c_pass) || empty($c_country) || empty($c_city) ||
+	empty($c_contact) || empty($c_address) || empty($c_image)){
+		
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><b>PLease Fill all fields..!</b>
+			</div>
+		";
+		exit();
+	} else {
+		if(!preg_match($name,$c_name)){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>this $c_name is not valid..!</b>
+			</div>
+		";
+		exit();
+	}
+	
+	if(!preg_match($emailValidation,$c_email)){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>this $c_email is not valid..!</b>
+			</div>
+		";
+		exit();
+	}
+	if(strlen($c_pass) < 9 ){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>Password is weak</b>
+			</div>
+		";
+		exit();
+	}
+	
+	if(!preg_match($number,$c_contact)){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>Mobile number $c_contact is not valid</b>
+			</div>
+		";
+		exit();
+	}
+	if(!(strlen($c_contact) == 10)){
+		echo "
+			<div class='alert alert-warning'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>Mobile number must be 10 digit</b>
+			</div>
+		";
+		exit();
+	}
+	//existing email address in our database
+	$sql = "SELECT customer_id  FROM  customers WHERE customer_email  = '$c_email' LIMIT 1" ;
+	$check_query = mysqli_query($con,$sql);
+	$count_email = mysqli_num_rows($check_query);
+	if($count_email > 0){
+		echo "
+			<div class='alert alert-danger'>
+				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+				<b>Email Address is already available Try Another email address</b>
+			</div>
+		";
+		exit();
+	} else {
+		$password = md5($c_pass);
+		move_uploaded_file($c_image_tmp,"customer/customer_images/$c_image");
+		$sql = "INSERT INTO `customers` 
+		(`customer_name`, `customer_email`, `customer_pass`, `customer_country`, 
+		`customer_city`, `customer_contact`, `customer_address`, `customer_image`, `customer_ip`) 
+		VALUES ('$c_name', '$c_email', '$c_pass', 
+		'$c_country', '$c_city', '$c_contact', '$c_address','$c_image','$c_ip')";
+		$run_query = mysqli_query($con,$sql);
+		$sel_cart = "select * from cart where ip_add='$c_ip'";
+    
+    $run_cart = mysqli_query($con,$sel_cart);
+    
+    $check_cart = mysqli_num_rows($run_cart);
+    
+    if($check_cart>0){
+        
+        /// If register have items in cart ///
+        
+        $_SESSION['customer_email']=$c_email;
+        
+        echo "<script>alert('You have been Registered Sucessfully')</script>";
+        
+        echo "<script>window.open('checkout.php','_self')</script>";
+        
+    }else{
+        
+        /// If register without items in cart ///
+        
+        $_SESSION['customer_email']=$c_email;
+        
+        echo "<script>alert('You have been Registered Sucessfully')</script>";
+        
+        echo "<script>window.open('index.php','_self')</script>";
+        
+    }
+	}
+	}
+	
+}
+?>
---
+++ index.php
@@ -0,0 +1,198 @@
+<?php 
+
+    $active='Home';
+    include("includes/header.php");
+
+?>
+   
+   <div class="container" id="slider"><!-- container Begin -->
+       
+       <div class="col-md-12"><!-- col-md-12 Begin -->
+           
+           <div class="carousel slide" id="myCarousel" data-ride="carousel"><!-- carousel slide Begin -->
+               
+               <ol class="carousel-indicators"><!-- carousel-indicators Begin -->
+                   
+                   <li class="active" data-target="#myCarousel" data-slide-to="0"></li>
+                   <li data-target="#myCarousel" data-slide-to="1"></li>
+                   <li data-target="#myCarousel" data-slide-to="2"></li>
+                   <li data-target="#myCarousel" data-slide-to="3"></li>
+                   
+               </ol><!-- carousel-indicators Finish -->
+               
+               <div class="carousel-inner"><!-- carousel-inner Begin -->
+                  
+                  <?php 
+                   
+                   $get_slides = "select * from slider LIMIT 0,1";
+                   
+                   $run_slides = mysqli_query($con,$get_slides);
+                   
+                   while($row_slides=mysqli_fetch_array($run_slides)){
+                       
+                       $slide_name = $row_slides['slide_name'];
+                       $slide_image = $row_slides['slide_image'];
+                       $slide_url = $row_slides['slide_url'];
+                       
+                       echo "
+                      
+                       <div class='item active'>
+                       
+                           <a href='$slide_url'>
+
+                                <img src='admin_area/slides_images/$slide_image'>
+								
+                                    <div class='centered1'><h1>our  products will close the competitive life of human beigns with mother nature</h1></div>
+                           </a>
+                       
+                       </div>
+                         
+                       ";
+                       
+                   }
+                   
+                   $get_slides = "select * from slider LIMIT 1,3";
+                   
+                   $run_slides = mysqli_query($con,$get_slides);
+                   
+                   while($row_slides=mysqli_fetch_array($run_slides)){
+                       
+                       $slide_name = $row_slides['slide_name'];
+                       $slide_image = $row_slides['slide_image'];
+                       $slide_url = $row_slides['slide_url'];
+                       
+                       echo "
+                       
+                       <div class='item'>
+                       
+                           <a href='$slide_url'>
+
+                                <img src='admin_area/slides_images/$slide_image'>
+                                 <div class='centered1'><h1>We implement new technologies for agriculture growth</h1></div>
+                           </a>
+                       
+                       </div>
+                       
+                       ";
+                       
+                   }
+                   
+                   ?>
+                   
+               </div><!-- carousel-inner Finish -->
+               
+               
+               
+               
+               
+           </div><!-- carousel slide Finish -->
+           
+       </div><!-- col-md-12 Finish -->
+       
+   </div><!-- container Finish -->
+   
+   <div id="advantages"><!-- #advantages Begin -->
+       
+       <div class="container"><!-- container Begin -->
+           
+           <div class="same-height-row"><!-- same-height-row Begin -->
+
+           <?php 
+           
+           $get_boxes = "select * from boxes_section";
+           $run_boxes = mysqli_query($con,$get_boxes);
+
+           while($run_boxes_section=mysqli_fetch_array($run_boxes)){
+
+            $box_id = $run_boxes_section['box_id'];
+            $box_title = $run_boxes_section['box_title'];
+            $box_desc = $run_boxes_section['box_desc'];
+           
+           ?>
+               
+               <div class="col-sm-4"><!-- col-sm-4 Begin -->
+                   
+                   <div class="box2 same-height"><!-- box same-height Begin -->
+                       
+                       <div class="icon"><!-- icon Begin -->
+                           
+                           <i class="fa fa-heart"></i>
+                           
+                       </div><!-- icon Finish -->
+                       
+                       <h3><a href="#"><?php echo $box_title; ?></a></h3>
+                       
+                       <p> <?php echo $box_desc; ?> </p>
+                       
+                   </div><!-- box same-height Finish -->
+                   
+               </div><!-- col-sm-4 Finish -->
+
+            <?php    } ?>
+               
+           </div><!-- same-height-row Finish -->
+           
+       </div><!-- container Finish -->
+       
+   </div><!-- #advantages Finish -->
+   
+   <div id="hot"><!-- #hot Begin -->
+       
+       <div class="box1"><!-- box Begin -->
+           
+           <div class="container"><!-- container Begin -->
+               
+               <div class="col-md-12"><!-- col-md-12 Begin -->
+                   
+                   <h2>
+                       Our Latest Products
+                   </h2>
+                   
+               </div><!-- col-md-12 Finish -->
+               
+           </div><!-- container Finish -->
+           
+       </div><!-- box Finish -->
+       
+   </div><!-- #hot Finish -->
+   
+   <div id="content" class="container"><!-- container Begin -->
+       
+       <div class="row"><!-- row Begin -->
+          
+          <?php 
+           
+           getPro();
+           
+           ?>
+           
+       </div><!-- row Finish -->
+       
+   </div><!-- container Finish -->
+   
+   <?php 
+    
+    include("includes/footer.php");
+    
+    ?>
+    
+    
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5e93640835bcbb0c9ab04942/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+    
+    
+</body>
+</html>
